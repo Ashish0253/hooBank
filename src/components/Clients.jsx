a521fef85e32b858a4297b063d29c5984a8bb57d
Ashish0253 <ashishrana0253@gmail.com>
2023-04-04 21:44:55 +0530

    CTA completed

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,3 +1,25 @@
-export default function Clients() {
-  return <div>Clients</div>;
-}
+import styles from "../style";
+import { clients } from "../constants";
+
+const Clients = () => {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:w-[192px] min-w-[120px] `}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="sm:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Clients;
